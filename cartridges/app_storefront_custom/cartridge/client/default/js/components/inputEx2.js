2d33c255de83009a86d76435f77b5e16dcd8ad9e
raresdorneanu <rares.dorneanu.rd@gmail.com>
2023-03-14 19:06:50 +0200

    show coupon form using JS

---
+++ cartridges/app_storefront_custom/cartridge/client/default/js/components/inputEx2.js
@@ -0,0 +1,11 @@
+var couponCodeLink = document.getElementById("coupon-code-link");
+var couponForm = document.querySelector(".promo-code-form");
+couponForm.style.display = "none";
+
+couponCodeLink.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (couponForm.style.display === "none") {
+        couponForm.style.display = "block";
+        couponCodeLink.style.display = "none";
+    }
+});
